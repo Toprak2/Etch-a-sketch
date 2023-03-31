d866276c7237dd49bcee8d212c92b28649c73d1e
Muhammed Ali <muhammedalitoprak1@gmail.com>
2023-04-01 00:12:05 +0300

    Add event to clear button ,it removes the hover effect

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const mainGrid = document.querySelector(".main-grid");
-console.log(mainGrid.clientWidth);
 
-let padding =100;
+//create 16*16 grid as default otherwise create an enteredAmount*enteredAmount grid
 function createGrid(divAmount=16){
-
+    let padding =100;
     const squareWidth=(mainGrid.clientWidth-padding) / divAmount;
 
     for(let i=0;i<divAmount*divAmount;i++){
@@ -15,14 +14,16 @@ function createGrid(divAmount=16){
     }
     addHover();
 }   
-
+//When page loads create the default grid
 createGrid();
 
+//Clear the grid
 function resetGrid(){
     const squares = document.querySelectorAll(".square");
     squares.forEach(square => square.remove());
 }
 
+//add hover class to the divs
 function addHover(){
     const squares = document.querySelectorAll(".square");
 
@@ -31,19 +32,25 @@ function addHover(){
     }));
 }
 
+//add functionality to reset button
 const resetButton =document.querySelector(".reset");
 resetButton.addEventListener("click",function(){
     resetGrid();
     createGrid();
 });
 
+// ask user to input a number create a grid based on that number
 const newButton = document.querySelector(".new");
-
 newButton.addEventListener("click",function(){
-    let amount=prompt("enter the row number");
+    let amount=prompt("enter a number between 0-100");  
     if(!isNaN(amount)){
         amount = parseFloat(amount);
         if(Number.isInteger(amount)){
+            if(amount>100 || amount<0)
+            {
+                alert("Please enter a number between 0-100");
+                return;
+            }
             resetGrid();
             createGrid(amount);
         }
@@ -54,5 +61,13 @@ newButton.addEventListener("click",function(){
     else{
         alert("please enter an integer");
     }
-})
+});
+
+//remove the hover effect from the tiles 
+const clearButton =document.querySelector(".clear");
+clearButton.addEventListener("click",function(){
+    const hoveredSquares = document.querySelectorAll(".hover");
+    hoveredSquares.forEach(square => square.classList.remove("hover")); 
+});
+
 
